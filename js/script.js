442fc74daeb624465199d843306f8f61b8d5e931
ORTIZ <aidenortiz@mail.ru>
2022-07-09 15:53:10 +0300

    Добавлены py и js файлы

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 console.log("Hello world");
+console.log("Checking site....")
+console.log("Site is working")
+console.log("Site was made by ORTIZ, 2022")
---
+++ python/django.py
@@ -0,0 +1,3 @@
+hi = "Hello my friend! My name is ORTIZ (Nikita). And welcome on my project. I only starting, but i see a very big progress in my work. I will do my best"
+
+print(hi)
